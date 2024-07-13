ceeefcc0df6b1aa694e817c7c543d89e1397fe95
Beregszászi István <beistvan@gmail.com>
2024-07-13 08:19:16 +0200

    Add The Executor Framework - Class RunnableTask

---
+++ the-executor-framework-class-runnabletask.java
@@ -0,0 +1,18 @@
+public class RunnableTask implements Runnable {
+
+  private final long limit;
+  
+  // Constructor
+  RunnableTask(long limit) {
+    this.limit = limit;
+  }
+
+  @Override
+  public void run() {
+    long sum = 0;
+    for (long i = 1; i < limit; i++) {
+      sum += i;
+    }
+    System.out.println(Thread.currentThread().getName() + ": " + sum);
+  }
+}
