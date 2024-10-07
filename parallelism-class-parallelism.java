76070d8503383b752a692691a55970388770a070
Beregszászi István <beistvan@gmail.com>
2024-10-08 00:17:44 +0200

    Parallelism - Class Parallelism

---
+++ parallelism-class-parallelism.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.stream.*;
+  
+public class Parallelism {
+  public static void main(String args[]) {
+    List<String> welcome = Arrays.asList("Welcome","to","streams!");
+    welcome.stream().forEach(y->System.out.println(y));
+  }
+}
