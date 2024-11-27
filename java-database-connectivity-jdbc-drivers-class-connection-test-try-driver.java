72ca338a29bcacb15394719d23a5f23666349679
beistvan <beistvan@gmail.com>
2024-11-27 12:18:34 +0100

    Update java-database-connectivity-jdbc-drivers-class-connection-test-try-driver.java

--- java-database-connectivity-jdbc-drivers-class-connection-test-try-driver.java
+++ java-database-connectivity-jdbc-drivers-class-connection-test-try-driver.java
@@ -2,8 +2,12 @@ public class ConnectionTest {
   public static void main(String[] args) { 
     try {
       Class.forName("org.sqlite.JDBC");
+      System.out.println("The driver was successfully loaded.");
     } catch (ClassNotFoundException e) {
+      // Do something to handle error
       e.printStackTrace();
+      System.out.println("The driver class was not found in the program files at runtime.");
+      System.exit(1);
     }
   }
 }
