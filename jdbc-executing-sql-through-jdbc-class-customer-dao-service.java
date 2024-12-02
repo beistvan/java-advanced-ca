fa4f8ad64c230868b0cb0afe4c1d8e33380870a4
beistvan <beistvan@gmail.com>
2024-12-02 02:55:37 +0100

    Java Database Connectivity (JDBC) - Executing SQL Through JDBC - Class CustomerDaoService

---
+++ jdbc-executing-sql-through-jdbc-class-customer-dao-service.java
@@ -0,0 +1,98 @@
+package services;
+
+import models.Customer;
+
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerDaoService {
+  private static final String url = "jdbc:sqlite:resources/MYSTERY_BUSINESS.db";
+
+  /*
+  Method to test the drivers are found in the classpath
+   */
+  public static void loadDriver() {
+    try {
+      Class.forName("org.sqlite.JDBC");
+      System.out.println("The driver was successfully loaded.");
+    } catch (ClassNotFoundException e) {
+      System.out.println("The driver class was not found in the program files at runtime.");
+      System.out.println(e);
+      System.exit(1);
+    }
+  }
+
+  /*
+  Method to test the connection to the database
+   */
+  public static void testDatabaseConnection() {
+    try (Connection connection = DriverManager.getConnection(url)) {
+      System.out.println("The connection to the SQLite database was successful!");
+    } catch (SQLException e) {
+      System.out.println("The connection to the database was unsuccessful!");
+      System.out.println(e);
+    }
+  }
+
+  /*
+  Method to create the CUSTOMER table in the database
+   */
+  public static void createTable() {
+    // Edit SQL Query string here:
+    String createTableStatement = "CREATE TABLE CUSTOMERS ("
+      + "CUST_ID INTEGER PRIMARY KEY,"
+      + "CUST_F_NAME TEXT NOT NULL,"
+      + "CUST_L_NAME TEXT NOT NULL,"
+      + "CUST_EMAIL TEXT NOT NULL,"
+      + "CUST_CELL_NUM TEXT NOT NULL"
+      + ");";
+
+    try (
+      Connection connection = DriverManager.getConnection(url);
+      Statement statement = connection.createStatement();
+    ) {
+      // Add .executeUpdate() method here:
+      statement.executeUpdate(createTableStatement);
+      System.out.println("The CUSTOMER table has been created.");
+    } catch (SQLException e) {
+      System.out.println("There was an error with your request.");
+      System.out.println(e);
+    }
+  }
+
+  /*
+  Method to save a list of customers to the CUSTOMER table
+   */
+  public static void saveCustomers(List<Customer> customers) {
+    try (
+      Connection connection = DriverManager.getConnection(url);
+      Statement statement = connection.createStatement();
+    ) {
+      System.out.println("The customers have been saved to the CUSTOMER table.");
+    } catch (SQLException e) {
+      System.out.println("There was an error with your request.");
+      System.out.println(e);
+    }
+  }
+
+  /*
+  Method to load all customers from the CUSTOMER table
+   */
+  public static List<Customer> loadAllCustomers() {
+    try (
+      Connection connection = DriverManager.getConnection(url);
+      Statement statement = connection.createStatement();
+    ) {
+      System.out.println("All customers we loaded from the database.");
+    } catch (SQLException e) {
+      System.out.println("There was an error with your request.");
+      System.out.println(e);
+    }
+
+    return new ArrayList<>();
+  }
+}
